c59571de677de48a21a90035f9869d7bcaafbc96
Надежда Иванова <forstudyni@yandex.ru>
2026-01-15 21:35:43 +0900

    task 1.1

---
+++ task1/1.1/script.js
@@ -0,0 +1,5 @@
+const name = prompt('Введите своё имя');
+const surname = prompt('Введите свою фамилию');
+const age = prompt('Введите свой возраст');
+
+console.log(`Привет, ${name} ${surname}! Тебе ${age} лет.`)
